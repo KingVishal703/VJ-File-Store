adc6a84820b8e02e9ba16297e333a7fce80f0970
KingVishal703 <vishal21wankhede@gmail.com>
2024-11-05 12:35:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -240,7 +240,7 @@ async def start(client, message):
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
-            title = 'https://telegram.me/Anime_movies_V  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
+            title = '@Anime_movies_V  ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), file.file_name.split()))
             size=get_size(file.file_size)
             f_caption = f"<code>{title}</code>"
             if CUSTOM_FILE_CAPTION:
